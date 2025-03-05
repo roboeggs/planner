4c3d7715ba5c5f2144ccf77e8d883b27edf348b6
Andrei <andfishka@gmail.com>
2025-03-05 15:51:16 +0300

    style: update CSS for button styling and layout adjustments; enhance EJS template for task and calendar rendering

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,24 @@ const helpers = require('./helpers');
 
 var router = express.Router();
 
+const refactoringJSON = (data) => {
+  let json_data = JSON.parse(data);
+  for (const task of json_data.subtasks) {
+    task.start_date = new Date(task.start_date).getUTCDate();
+    task.due_date = new Date(task.due_date).getUTCDate();
+    console.log(task.start_date);
+  }
+  return json_data;
+}
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   // for (const task of encoded["subtasks"]) {
   //   console.log(task);
   // }
-  res.render('index', { title: 'Express', data: encoded, getColorByComplexity: helpers.getColorByComplexity });
+  res.render('index', { title: 'Express', data: refactoringJSON(encoded), getColorByComplexity: helpers.getColorByComplexity });
 });
 
 module.exports = router;
